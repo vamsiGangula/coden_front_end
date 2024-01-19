a00c59dd205daff301026f2a175dc07187808502
vamsiGangula <vamsigangula9133@gmail.com>
2024-01-19 11:22:05 +0530

    fix the issues

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Register from './pages/register';
-import Charts from './pages/charts';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Register from "./pages/register";
+import Charts from "./pages/charts";
 
 function App() {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Register />} />
-    //     <Route path="/charts" element={<Charts />} />
-    //   </Routes>
-    // </Router>
     <>
-    <h1>Register</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/charts" element={<Charts />} />
+        </Routes>
+      </Router>
     </>
   );
 }
